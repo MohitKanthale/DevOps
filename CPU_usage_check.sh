5a1a8008b503b0654aeaa3e12c1472a1ef7f697d
Mohit-1201 <mohitkanthale9@gmail.com>
2025-08-06 18:42:00 +0530

    new file created CPU_usage_check.sh

---
+++ CPU_usage_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#here in place of bandaru, use your local system user-name
+
+#set -exo
+
+#use mail cmd in linux to check the mails
+
+THRESHOLD=80
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+if (( $(echo "$cpu_usage > $THRESHOLD" | bc -l) )); then
+    echo "CPU usage is above $THRESHOLD%. Current usage: $cpu_usage%" | mail -s "CPU Alert" bandaru
+else
+        echo "cpu usage is below 80" | mail -s "CPU Alert" bandaru
+fi
