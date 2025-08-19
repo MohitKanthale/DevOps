d9210289a7f12c1237c87825a8f183c011fbf900
MohitKanthale <mohitkanthale82@gmail.com>
2025-08-19 12:36:38 +0530

    added the loop syntax

--- Shell-Scripts/Basic_loops.sh
+++ Shell-Scripts/Basic_loops.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Bash has no explicit data types.
+
+#Variables are treated as strings by default, but you can perform arithmetic operations using (( )):
+
 var="value"
 
 if [ "$var" == "value" ]; then
@@ -7,3 +11,20 @@ if [ "$var" == "value" ]; then
 else
     echo "No match!"
 fi
+
+
+#For Loop
+
+for fruit in apple banana mango; do
+    echo "I like Mango"
+done
+
+
+#While loop
+
+count=0
+
+while [ $count -lt 5 ]; do
+    echo "Count: $count"
+    ((count++))
+done
