11fbaada25cf4e1e0f44b6fa8a4b38eab9e0600b
MohitKanthale <mohitkanthale82@gmail.com>
2025-08-19 11:05:08 +0530

    rough change

--- sample_create_file.sh
+++ sample_create_file.sh
@@ -2,7 +2,7 @@
 FILE_NAME="my_log.txt"
 echo "Creating a new file: $FILE_NAME"
 touch "$FILE_NAME"
-echo "Adding content to $FILE_NAME"
+echo "Adding content to the $FILE_NAME"
 echo "This is a Log entry." >> "$FILE_NAME"
 echo "Displaying content of $FILE_NAME:"
 cat "$FILE_NAME"
