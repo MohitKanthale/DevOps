05458cefe7492c0273e958acba25e710e1d623fa
MohitKanthale <mohitkanthale82@gmail.com>
2025-09-02 18:47:05 +0530

    add

--- CPU_usage_check.sh
+++ CPU_usage_check.sh
@@ -2,7 +2,7 @@
 
 #here in place of bandaru, use your local system user-name
 
-#set -exo for debugging
+#set -exo
 
 #use mail cmd in linux terminal to check the mails
 
@@ -11,5 +11,5 @@ cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk
 if (( $(echo "$cpu_usage > $THRESHOLD" | bc -l) )); then
     echo "CPU usage is above $THRESHOLD%. Current usage: $cpu_usage%" | mail -s "CPU Alert" bandaru
 else
-        echo "cpu usage is below 80" | mail -s "CPU Alert" bandaru
+    echo "cpu usage is below 80" | mail -s "CPU Alert" bandaru
 fi
