f5a95a24fa536bf0ccd5b66cff7f50502d8f3954
MohitKanthale <mohitkanthale82@gmail.com>
2025-09-23 12:46:04 +0530

    comment added to check total avail ram

--- Shell-Scripts/Ram_Check.sh
+++ Shell-Scripts/Ram_Check.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Here, free ram is calculated using total of "free + Swap".
-
+# But, for calculating specific free ram use below line
+# "FREE_MEM=$(free -mt | grep Mem | awk '{print $7}')"   --- It will show total avialable ram.
 
 FREE_MEM=$(free -mt | grep Total | awk '{print $4}')
 TH=100
